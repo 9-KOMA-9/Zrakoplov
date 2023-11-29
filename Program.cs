f1e15dd8959356cbc22d1330b61fc4327d636629
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-11-29 09:33:08 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlaseZrakoplov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zrakoplov zrakoplov = new Zrakoplov();
+            Zrakoplov.setNaziv("Airbus");
+        }
+    }
+}
